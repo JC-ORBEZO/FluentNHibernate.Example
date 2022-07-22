77fe84212576408bb4dcbad64ccb0a15eb84304f
JC-ORBEZO <jose.orbezo@alumnos.frgp.utn.edu.ar>
2022-07-22 15:18:23 -0300

    Agregar archivos de proyecto.

---
+++ FluentNHibernate.Example/NHibernateHelper.cs
@@ -0,0 +1,48 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentNHibernate.Example
+{
+    public class NHibernateHelper
+    {
+        private static ISessionFactory _sessionFactory;
+        private static ISessionFactory SessionFactory
+        {
+            get
+            {
+                if (_sessionFactory == null)
+                    InitializeSessionFactory();
+
+                return _sessionFactory;
+            }
+        }
+
+        private static void InitializeSessionFactory()
+        {
+            _sessionFactory = Fluently.Configure()
+                .Database(MsSqlConfiguration.MsSql2008
+                    .ConnectionString(
+                        @"Server=localhost\SQLExpress;Database=SimpleNHibernate;Trusted_Connection=True;")
+                    .ShowSql()
+                )
+                .Mappings(m =>
+                          m.FluentMappings
+                            .AddFromAssemblyOf<Car>())
+                .ExposeConfiguration(cfg => new SchemaExport(cfg)
+                                                .Create(true, true))
+                .BuildSessionFactory();
+        }
+
+        public static ISession OpenSession()
+        {
+            return SessionFactory.OpenSession();
+        }
+    }
+}
